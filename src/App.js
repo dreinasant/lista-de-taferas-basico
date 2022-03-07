9f920cd8c860010a4d95535635ea4f65f802d1aa
dreinasant <ao.andreina@gmail.com>
2022-03-06 23:30:22 -0300

    desafio-todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class todo extends Component {
+
+  state = {
+    tarefa:'',
+    lista: []
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      tarefa: event.target.value
+    })
+  }
+
+  add = (event) =>{
+    let {lista, tarefa} = this.state  //desconstrução
+    if(tarefa != ""){
+      this.setState({
+        tarefa: '',
+        lista: lista.concat({
+          tarefa: tarefa,
+          id: Date.now()
+        })
+      })
+    }
+    event.preventDefault()
+  }
+
+  remover = (id) => {
+    let {lista} = this.state
+    this.setState({
+      lista: lista.filter((item)=> {
+        return item.id != id
+      })
+    })
+  }
+
+  render(){
+    return(
+      <div className="box">
+        <form onSubmit={this.add}>
+        <h1>Lista de tarefa basico</h1>
+        <div className="intro-box">
+        <input onChange={this.handleChange} type="text" value={this.state.tarefa}/>
+        <button onClick={this.add}>Add</button>
+        <div className="contain">
+          {this.state.lista.map((item) =>(
+            <ul>
+              <li>{item.tarefa} <button onClick={()=>{this.remover(item.id)}}>X</button></li>
+            </ul>
+          ))}
+        </div>
+      </div>
+      </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default todo;
